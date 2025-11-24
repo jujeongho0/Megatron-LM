0dbb9c586ef9a1bfa9bc3e15b03cab9f35ab7aa4
Jeongho Ju <jujeongho@ajou.ac.kr>
2025-11-24 19:31:48 +0900

    Update export_megatron_to_hf.py

--- bridge/export_megatron_to_hf.py
+++ bridge/export_megatron_to_hf.py
@@ -35,6 +35,8 @@ def load_megatron_model(megatron_path, hf_model):
     mlm_args.context_parallel_size = 1
     mlm_args.transformer_pipeline_model_parallel_size = 1
 
+    mlm_args.recompute_granularity = None
+
     # with torch.device("meta"):
     pre_process = parallel_state.is_pipeline_first_stage()
     post_process = parallel_state.is_pipeline_last_stage()
