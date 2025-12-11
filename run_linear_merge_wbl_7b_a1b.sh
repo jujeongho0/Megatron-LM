85c784d6f0c659bfb06ae3de261aec1ea7a08b25
Jeongho Ju <jujeongho@ajou.ac.kr>
2025-12-11 19:34:10 +0900

    Update run_linear_merge_wbl_7b_a1b.sh

--- run_linear_merge_wbl_7b_a1b.sh
+++ run_linear_merge_wbl_7b_a1b.sh
@@ -14,14 +14,14 @@ python3 -m torch.distributed.run \
   --nproc_per_node=${NUM_GPUS} \
   ${SCRIPT} \
   \
-  --disable-bias-linear \
+  ---disable-bias-linear \
   --seq-length 4096 \
   --max-position-embeddings 32768 \
   --num-layers 24 \
   --position-embedding-type none \
-  --hidden-size 2048 \
-  --ffn-hidden-size 9216 \
-  --num-attention-heads 16 \
+  --hidden-size 1536 \
+  --ffn-hidden-size 5760 \
+  --num-attention-heads 12 \
   --attention-dropout 0.0 \
   --hidden-dropout 0.0 \
   --swiglu \
@@ -29,9 +29,9 @@ python3 -m torch.distributed.run \
   \
   --num-experts 64 \
   --moe-layer-freq "([0]*1+[1]*23)" \
-  --moe-ffn-hidden-size 1024 \
-  --moe-shared-expert-intermediate-size 1024 \
-  --moe-router-topk 8 \
+  --moe-ffn-hidden-size 960 \
+  --moe-shared-expert-intermediate-size 960 \
+  --moe-router-topk 5 \
   --moe-router-score-function sigmoid \
   --moe-router-dtype fp32 \
   --moe-token-dispatcher-type alltoall \
@@ -42,7 +42,7 @@ python3 -m torch.distributed.run \
   --sliding-window-interleave-k 6 \
   \
   --multi-latent-attention \
-  --q-lora-rank 1024 \
+  --q-lora-rank 768 \
   --kv-lora-rank 512 \
   --qk-head-dim 128 \
   --qk-pos-emb-head-dim 64 \
